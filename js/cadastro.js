dda7ccc5850d33fa15e5a667acdb2f98719862b7
Matheus Ventura Nellessen <ti.matheus.v.n@gmail.com>
2025-06-17 19:08:31 -0300

    feat: adiciona constantes via DOM

---
+++ js/cadastro.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('form-necessidade');
+  const msgErro = document.getElementById('msg-erro');
+  const msgSucesso = document.getElementById('msg-sucesso');
+  const cepInput = document.getElementById('cep');
+});
