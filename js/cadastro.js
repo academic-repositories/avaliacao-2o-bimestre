99b88d165fce4ee3716b4cd304dc72c515f11cb6
Matheus Ventura Nellessen <ti.matheus.v.n@gmail.com>
2025-06-17 19:10:32 -0300

    feat: nova funcionalidade em cadastro.js
    Limpa mensagens ao focar em qualquer outro campo

--- js/cadastro.js
+++ js/cadastro.js
@@ -3,4 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
   const msgErro = document.getElementById('msg-erro');
   const msgSucesso = document.getElementById('msg-sucesso');
   const cepInput = document.getElementById('cep');
+
+  form.querySelectorAll('input, select, textarea').forEach(el => {
+    el.addEventListener('focus', () => {
+      msgErro.textContent = '';
+      msgSucesso.textContent = '';
+    });
+  });
+  
 });
